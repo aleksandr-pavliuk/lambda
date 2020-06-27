68af75ccf4c61763c1c1fe83c8e993636639eec9
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2020-06-27 14:36:07 +0300

    implement Consumer example

---
+++ src/main/java/ua/org/alex/lambda/features/consumer/ConsumerExample.java
@@ -0,0 +1,18 @@
+package ua.org.alex.lambda.features.consumer;
+
+import java.util.function.Consumer;
+
+/**
+ * @author Alex
+ * @link http://healthfood.net.ua
+ */
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        Consumer<String> con1 = (s) -> {System.out.println(s.toUpperCase());};
+        //con1.accept("java8");
+        Consumer<String> con2 = (s) -> System.out.println(s.toLowerCase());
+        //con2.accept("java8");
+        con1.andThen(con2).accept("jaVa8");
+    }
+}
