d53f9e7ccfcf783c17bb7e79881a0ba7fd660530
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2020-07-18 19:22:20 +0300

    implement LambdaLocalVariable

---
+++ src/main/java/ua/org/alex/lambda/features/methodref/LambdaLocalVariable.java
@@ -0,0 +1,26 @@
+package ua.org.alex.lambda.features.methodref;
+
+import java.util.function.Consumer;
+
+/**
+ * @author Alex
+ * @link http://healthfood.net.ua
+ */
+public class LambdaLocalVariable {
+
+    private static int k = 0;
+
+    public static void main(String[] args) {
+
+        int i = 10;
+
+        Consumer<Integer> c1 = (j) -> {
+//			i++;
+            k = 15;
+            System.out.println("Print K:" + k);
+            System.out.println("Value is :" + (i + j));
+        };
+
+        c1.accept(15);
+    }
+}
