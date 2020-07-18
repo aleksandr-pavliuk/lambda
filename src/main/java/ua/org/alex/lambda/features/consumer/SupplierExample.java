27b84c3b4b9e99134cd8020ee36f0c0c5d32f67f
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2020-07-18 18:31:29 +0300

    implement SupplierExample

--- src/main/java/ua/org/alex/lambda/features/consumer/SupplierExample.java
+++ src/main/java/ua/org/alex/lambda/features/consumer/SupplierExample.java
@@ -1,15 +1,23 @@
 package ua.org.alex.lambda.features.consumer;
 
+import ua.org.alex.lambda.features.repo.Person;
+import ua.org.alex.lambda.features.repo.PersonRepository;
+
+import java.util.List;
+import java.util.function.Supplier;
+
 /**
  * @author Alex
  * @link http://healthfood.net.ua
  */
 public class SupplierExample {
 
-    private
+    private static Supplier<Person> s1 = () -> PersonRepository.getPerson();
+    private static Supplier<List<Person>> s2 = () -> PersonRepository.getAllPersons();
 
     public static void main(String[] args) {
 
-
+        System.out.println("Result :"+s1.get());
+        System.out.println("Result :"+s2.get());
     }
 }
