51be1b943d14e60696c9b451d22322938d0efacf
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2020-07-18 18:28:23 +0300

    implement UnaryBinaryExample

---
+++ src/main/java/ua/org/alex/lambda/features/consumer/SupplierExample.java
@@ -0,0 +1,15 @@
+package ua.org.alex.lambda.features.consumer;
+
+/**
+ * @author Alex
+ * @link http://healthfood.net.ua
+ */
+public class SupplierExample {
+
+    private
+
+    public static void main(String[] args) {
+
+
+    }
+}
